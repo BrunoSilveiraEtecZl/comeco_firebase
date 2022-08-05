d945f503517ea40fa11ab95075c03a5d8342155b
FATEC ZONA LESTE <bruno.dionisio@etec.sp.gov.br>
2022-08-05 17:58:07 -0300

    first commit

---
+++ app/src/main/java/com/example/applogin/resposta.kt
@@ -0,0 +1,11 @@
+package com.example.applogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class resposta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resposta)
+    }
+}
